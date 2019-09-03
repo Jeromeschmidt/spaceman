ad9d2e32bef74819e884b47bdf5bbb211c2c8d02
jeromeschmidt <schmidt.jerome@yahoo.com>
2019-09-03 15:59:19 -0700

    finished most of functionality

--- spaceman.py
+++ spaceman.py
@@ -1,5 +1,7 @@
 import random
 
+letters_guessed = list()
+
 def load_word():
     '''
     A function that reads a text file of words and randomly selects one to use as the secret word
@@ -25,7 +27,10 @@ def is_word_guessed(secret_word, letters_guessed):
         bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
     '''
     # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
-    pass
+    for i in letters_guessed:
+        if(secret_word.find(i) == -1):
+            return False
+    return True
 
 def get_guessed_word(secret_word, letters_guessed):
     '''
@@ -38,8 +43,14 @@ def get_guessed_word(secret_word, letters_guessed):
     '''
 
     #TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
-
-    pass
+    word_in_progress = list()
+    for i in secret_word:
+        if(letters_guessed.find(i, 0, len(secret_word)) != -1):
+            word_in_progress.append(i)
+        else:
+            word_in_progress.append("_")
+    result = "".join(word_in_progress)
+    return result
 
 
 def is_guess_in_word(guess, secret_word):
@@ -52,11 +63,18 @@ def is_guess_in_word(guess, secret_word):
         bool: True if the guess is in the secret_word, False otherwise
     '''
     #TODO: check if the letter guess is in the secret word
+    if(secret_word.find(guess) != -1):
+        return True
+    return False
 
+def get_guess():
+    guess = input("Please enter 1 letter: ")
+    if(len(guess) != 1):
+        print("Please enter only 1 letter.")
+        get_guess()
+    else:
+        letters_guessed.append(guess)
     pass
-
-
-
 
 def spaceman(secret_word):
     '''
@@ -64,20 +82,26 @@ def spaceman(secret_word):
     Args:
       secret_word (string): the secret word to guess.
     '''
-
-
     #TODO: show the player information about the game according to the project spec
 
     #TODO: Ask the player to guess one letter per round and check that it is only one letter
+    guess = get_guess()
+    #letters_guessed.append(guess)
 
     #TODO: Check if the guessed letter is in the secret or not and give the player feedback
-
+    is_guess_in_word(guess, secret_word)
     #TODO: show the guessed word so far
-
+    letters_guessed_to_pass = "".join(letters_guessed)
+    result = get_guessed_word(secret_word, letters_guessed_to_pass)#"abcdefghijklmnopqurstuvwxyz")
+    print(letters_guessed_to_pass)
     #TODO: check if the game has been won or lost
-
-
-
+    if(result.find("_") == -1):
+        print("GAME WON")
+        print("Word was: " + secret_word)
+    elif(len(letters_guessed_to_pass) < 7):
+        spaceman(secret_word)
+    else:
+        print("Sorry you didnt win...")
 
 
 
